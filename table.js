99f9de2077b1eda1e9c477f1ad7094348b23aa72
Pankaj Jain <capankajjain@smilyo.com>
2019-12-07 08:28:50 +0530

    releasing as opensource

--- table.js
+++ table.js
@@ -1,22 +1,3 @@
-/*************************************************************************
- *
- * TaxHeal.in CONFIDENTIAL
- * __________________
- *
- *  [2017] - [2025] TaxHeal.in
- *  All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of TaxHeal.in and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to TaxHeal.in
- * and its suppliers and may be covered by India and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from TaxHeal.in
- */
-
 define([], function() {
 
     function createTable(divId, tableId, headData, tableRowsHTML) {
